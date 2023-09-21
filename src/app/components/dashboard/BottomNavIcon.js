d40f4bbf9f996d0394dd25936a7fe6e831fa93df
geekyasif <mohdasif.dev@gmail.com>
2023-09-21 19:15:00 +0530

    bottom navbar fix

--- src/app/components/dashboard/BottomNavIcon.js
+++ src/app/components/dashboard/BottomNavIcon.js
@@ -1,18 +1,22 @@
 "use client";
 
 import Link from "next/link";
-import {  usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function BottomNavIcon({ text, icon, url }) {
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   return (
-    <div className="p-1">
+    <div className="p-1 flex justify-center flex-col items-center">
       <Link
         href={`/${url}`}
-        className={`text-xs text-white text-center flex flex-col items-center ${pathname === `/${url}` && "font-bold"}`}
+        className={`text-xs text-white text-center flex flex-col items-center ${
+          pathname === `/${url}` && "font-bold"
+        }`}
       >
-        <span className="mb-1"> {icon}</span>
+        <div className=" w-full text-center flex justify-center mb-1">
+          <p className="inline w-[20px]">{icon}</p>
+        </div>
         {text}
       </Link>
     </div>
